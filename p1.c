cbbedd2bd11fc1db21ab2790cb9281428c618b24
Jing <3496271455@qq.com>
2026-08-08 17:22:01 +0800

    add

---
+++ p1.c
@@ -0,0 +1,28 @@
+// 
+
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < N; i++)
+    {
+        if (pid == 0)
+        {
+            printf("");
+            sleep(20);
+            exit(0);
+        }
+        
+    }
+    
+    for (int i = 0; i < N; i++)
+    {
+        wait(NULL);
+
+    }
+    printf("");
+
+
+
+
+    return 0;
+}
---
+++ p2.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    if (argc < 2)
+    {
+        printf("参数不足\n");
+        return 0;
+    }
+    printf("这是一个程序\n");
+    return 0;
+}
